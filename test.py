02cd071e1855cca3ec3f64224991d8aa9db583d3
ksprudhvi <ksprudhviofficial@gmail.com>
2024-11-18 23:35:48 -0700

    Profile

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ chrome_options.add_argument("--remote-debugging-port=9222")  # Enable debugging
 
 
 # Use an existing Chrome profile
-chrome_options.add_argument("user-data-dir=/.config/google-chrome/Profile 5") # Replace 'your-username'
+chrome_options.add_argument("user-data-dir=/.config/google-chrome") # Replace 'your-username'
 chrome_options.add_argument("profile-directory=Default")
 
 # Set the path to Chrome binary and ChromeDriver
