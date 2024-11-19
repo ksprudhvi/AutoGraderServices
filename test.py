c8f07d66bc527df9b96337f792681091754e4a02
ksprudhvi <ksprudhviofficial@gmail.com>
2024-11-18 23:52:55 -0700

    Display

--- test.py
+++ test.py
@@ -8,12 +8,13 @@ import time
 
 # Configure Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run without GUI
+#chrome_options.add_argument("--headless")  # Run without GUI
 chrome_options.add_argument("--disable-gpu")  # Disable GPU usage
 chrome_options.add_argument("--no-sandbox")  # Bypass sandbox restrictions
 chrome_options.add_argument("--disable-dev-shm-usage")  # Use /tmp instead of /dev/shm
 chrome_options.add_argument("--remote-debugging-port=9222")  # Enable debugging
-
+import os
+os.environ["DISPLAY"] = ":99"
 
 # Use an existing Chrome profile
 
