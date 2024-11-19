b5b7f3753fa8b81310f316a51c4b3efe50741032
ksprudhvi <ksprudhviofficial@gmail.com>
2024-11-18 22:37:59 -0700

    headless

--- test.py
+++ test.py
@@ -8,11 +8,12 @@ import time
 
 # Configure Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--headless")  # Enable headless mode if desired
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("window-size=1920x1080")
+chrome_options.add_argument("--headless")  # Run without GUI
+chrome_options.add_argument("--disable-gpu")  # Disable GPU usage
+chrome_options.add_argument("--no-sandbox")  # Bypass sandbox restrictions
+chrome_options.add_argument("--disable-dev-shm-usage")  # Use /tmp instead of /dev/shm
+chrome_options.add_argument("--remote-debugging-port=9222")  # Enable debugging
+
 
 # Use an existing Chrome profile
 chrome_options.add_argument("/usr/bin/google-chrome")
