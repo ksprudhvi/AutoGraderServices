3da1db707e9286204094c15e4646038b2dc3a957
ksprudhvi <ksprudhviofficial@gmail.com>
2024-11-18 22:33:34 -0700

    headless

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import time
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--headless")  # Enable headless mode if desired
+chrome_options.add_argument("--headless")  # Enable headless mode if desired
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("window-size=1920x1080")
 
